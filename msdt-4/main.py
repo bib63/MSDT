dc070d095ce9df0636e35be44110c5d80dfd8d08
Nikita <unekita111@gmail.com>
2024-12-22 12:31:19 +0400

    reformat logging inf the file

--- msdt-4/main.py
+++ msdt-4/main.py
@@ -3,7 +3,7 @@ from exceptions import (
     ProductNotFoundError, InvalidQuantityError,
     ShelfNotFoundError, OutOfStockError
 )
-
+from loguru import logger
 
 def create_order(product_id, quantity):
     # Проверка корректности количества продукции
@@ -12,14 +12,14 @@ def create_order(product_id, quantity):
 
     # Создание заказа
     order_id = 1  # Заглушка для простоты примера
-    print(f"Создан заказ {order_id} на продукцию {product_id} в количестве {quantity}.")
+    logger.info(f"Создан заказ {order_id} на {product_id} в количестве {quantity}.")
     return order_id
 
 
 def process_order(order_id):
     # Обработка заказа
     shelf_ids = [1, 2]  # Заглушка для простоты примера
-    print(f"Заказ {order_id} обработан и распределен на лавки {shelf_ids}.")
+    logger.info(f"Заказ {order_id} обработан и распределен на лавки {shelf_ids}.")
     return shelf_ids
 
 
@@ -29,7 +29,10 @@ def place_product(shelf_id, product_id, quantity):
         raise ShelfNotFoundError("Лавка с указанным идентификатором не найдена.")
 
     # Размещение продукции на лавке
-    print(f"Продукция {product_id} в количестве {quantity} размещена на лавке {shelf_id}.")
+    logger.info(
+        (f"Продукция {product_id} в количестве "
+        f"{quantity} размещена на лавке {shelf_id}.")
+    )
 
 
 def purchase_product(shelf_id, product_id, quantity):
@@ -42,7 +45,12 @@ def purchase_product(shelf_id, product_id, quantity):
         raise OutOfStockError("Продукция отсутствует в наличии на данной лавке.")
 
     # Покупка продукции
-    print(f"Продукция {product_id} в количестве {quantity} куплена с лавки {shelf_id}.")
+    logger.info(
+        (
+            f"Продукция {product_id} в количестве "
+            f"{quantity} куплена с лавки {shelf_id}."
+        )
+    )
 
 
 def get_product_info(product_id):
@@ -55,10 +63,19 @@ def get_product_info(product_id):
     product = next((p for p in products if p.id == product_id), None)
     if product is None:
         raise ProductNotFoundError("Продукция с указанным идентификатором не найдена.")
-    print(f"Получена информация о продукции {product_id}: {product}")
+    logger.info(f"Получена информация о продукции {product_id}: {product}")
     return product
 
 def main():
+    # инициализируем логер
+    logger.add(
+        'logs/log.txt',
+        format='{time}---{level}---{message}',
+        rotation='10:00',
+        retention='3 day',
+        compression='zip',
+        level='DEBUG'
+    )
     try:
         # Создание заказа
         order_id = create_order(1, 10)
@@ -74,13 +91,16 @@ def main():
         purchase_product(1, 1, 3)
 
     except ProductNotFoundError as e:
-        print("Произошла ошибка:", e)
+        logger.info("Произошла ошибка:", e)
     except InvalidQuantityError as e:
-        print("Произошла ошибка:", e)
+        logger.info("Произошла ошибка:", e)
     except ShelfNotFoundError as e:
-        print("Произошла ошибка:", e)
+        logger.info("Произошла ошибка:", e)
     except OutOfStockError as e:
-        print("Произошла ошибка:", e)
+        logger.info("Произошла ошибка:", e)
+    except Exception as e:
+        print("Произошла внезапная ошибка. Мы уже работаем над этим")
+        logger.exception(f"Произошла внезапная ошибка: {e}")
 
 
 if __name__ == '__main__':
